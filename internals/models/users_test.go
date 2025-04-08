4e561efb033547571a50e25900095f9d6e7c6d92
Babatunde50 <babatundeololade50@gmail.com>
2025-04-09 00:50:14 +0100

    Add internal packages for models, validation, and assertions

---
+++ internals/models/users_test.go
@@ -0,0 +1,46 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/Babatunde50/snippet/internals/assert"
+)
+
+func TestUserModelExists(t *testing.T) {
+	tests := []struct {
+		name   string
+		userID int
+		want   bool
+	}{
+		{
+			name:   "Valid ID",
+			userID: 1,
+			want:   true,
+		},
+		{
+			name:   "Zero ID",
+			userID: 0,
+			want:   false,
+		},
+		{
+			name:   "Non-existent ID",
+			userID: 2,
+			want:   false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			if testing.Short() {
+				t.Skip("models: skipping integration test")
+			}
+
+			db := newTestDB(t)
+			m := UserModel{db}
+			exists, err := m.Exists(tt.userID)
+			assert.Equal(t, exists, tt.want)
+			assert.NilError(t, err)
+		})
+	}
+}
